690e0ebb94795ccad209432d1feaa957101a4229
Alp Karagöz <alpkaragoz17@ku.edu.tr>
2025-07-04 11:46:01 +0300

    fixed api endpoint names

--- src/main/java/com/piats/backend/controllers/UserController.java
+++ src/main/java/com/piats/backend/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import com.piats.backend.models.User;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @CrossOrigin(origins = "http://localhost:4200") //TODO
 public class UserController {
     private final UserService userService;
